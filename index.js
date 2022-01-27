bf86c403d98eac8651f3565ce1fc2014cdf0efa7
Eike Wientjes <42077358+MarkWatney15102@users.noreply.github.com>
2022-01-27 12:32:58 +0100

    DEV

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const fs = require('fs');
+const gphoto2 = require('gphoto2');
+const GPhoto = new gphoto2.GPhoto2();
+
+GPhoto.setLogLevel(1);
+GPhoto.on('log', function (level, domain, message) {
+    console.log(domain, message);
+});
+
+const app = express();
+const port = 43921;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/take-photo', (req, res) => {
+    console.log('test');
+    req.status(200).json({message: "done"});
+});
