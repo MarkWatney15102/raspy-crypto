7419a3a0cf1b1cc9203fdceeadb6af7e0cfbca04
Eike Wientjes <42077358+MarkWatney15102@users.noreply.github.com>
2022-01-27 12:24:14 +0100

    Basic structure

--- run.sh
+++ run.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-sudo apt install apache2
-sudo apt install ufw
-sudo apt install nodejs
+sudo apt install ufw -y
 
 sudo ufw allow 43921
 sudo ufw enable
+
+wget https://nodejs.org/download/release/v11.15.0/node-v11.15.0-linux-armv7l.tar.gz
+tar -xzf node-v11.15.0-linux-armv7l.tar.gz
+cd node-v11.15.0-linux-armv7l
+sudo cp -R * /usr/local/
+cd ..
+sudo rm -rf node-v11.15.0-linux-armv7l
+sudo rm node-v11.15.0-linux-armv7l.tar.gz
+
+sudo apt install apt gphoto2 -y
+
+sudo apt install libgphoto2-dev -y
+
+npm i   
