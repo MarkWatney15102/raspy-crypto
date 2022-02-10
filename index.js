66eea4b4f0664d33dd2b17eb60e0a9e2596b6489
Eike Wientjes <42077358+MarkWatney15102@users.noreply.github.com>
2022-02-10 12:31:52 +0100

    CHANGE

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 
-const fs = require('fs');
-const gphoto2 = require('gphoto2');
-const GPhoto = new gphoto2.GPhoto2();
-
-GPhoto.setLogLevel(1);
-GPhoto.on('log', function (level, domain, message) {
-    console.log(domain, message);
-});
-
 const app = express();
 const port = 43921;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/take-photo', (req, res) => {
-    console.log('test');
-    req.status(200).json({message: "done"});
+app.get('/crypto/:formCoin/:toCoin', (req, res) => {
+   const { fromCoin, toCoin } = req.params;
+
+   if (fromCoin && toCoin) {
+        const symbol = fromCoin + toCoin;
+        fetch('https://api.binance.com/api/v3/ticker/24hr?symbol=' + symbol)
+            .then(result => result.json())
+            .then((result) => {
+                const lastPrice = result.lastPrice;
+                const tradesOneDay = result.count;
+                const priceChangePercent = result.priceChangePercent;
+
+                res.status(200).json({lastPrice, tradesOneDay, priceChangePercent});
+        });
+   }
+
+   res.status(400).json({"message": "Missing parameter"});
 });
 
 app.listen(port, () => {
